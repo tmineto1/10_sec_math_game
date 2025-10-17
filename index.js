29792c0058f59c1ad74c1b57e57be9a10d1eaeba
Thomas Minetos <athanasios.minetos@gmail.com>
2025-10-17 13:13:22 -0500

    Initial commit, basic html, js and css test

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumberGenerator = function (size) {
+  return Math.ceil(Math.random() * size);
+}
+var questionGenerator = function () {
+  var question = {};
+  var num1 = randomNumberGenerator(10);
+  var num2 = randomNumberGenerator(10);
+  
+  question.answer = num1 + num2;
+  question.equation = String(num1) + " + " + String(num2);
+  
+  return question;
+}
