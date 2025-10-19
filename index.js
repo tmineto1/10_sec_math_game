24ad27718b660a80a06c3276c7e991dad27357cd
Thomas Minetos <athanasios.minetos@gmail.com>
2025-10-18 22:26:53 -0500

    Added slider, operator options, css

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ $(document).ready(function(){
   var timeLeft = 10;
   var score = 0;
   var highScore = 0;
+  var add = document.getElementById('add');
+  var subtract = document.getElementById('subtract');
+  var multiply = document.getElementById('multiply');
+  var divide = document.getElementById('divide');
+  var operator;
+  var numberLimit = 10;
 
+
+  $('#number-limit').on('input', function() {
+    numberLimit = Number($(this).val());
+    $('#number-display').text(numberLimit);
+  });
   
   var updateTimeLeft = function (amount) {
     timeLeft += amount;
@@ -22,6 +33,31 @@ $(document).ready(function(){
     $('#highScore').text(highScore);
     }
   };
+
+  var getOperator = function() {
+    let operators = [];
+    if (add.checked === true) {
+            operators.push('+');
+    }
+    if (subtract.checked === true) {
+            operators.push('-');
+    }
+    if (multiply.checked === true) {
+            operators.push('*');
+    }
+    if (divide.checked === true) {
+            operators.push('/');
+    }
+    if (operators.length === 0) {
+            operator = '+';
+    } else if (operators.length === 1) {
+            operator = operators[0];
+    } else {
+            let i = Math.floor(Math.random() * operators.length);
+            operator = operators[i];
+    }
+    return operator;
+  }
 
   var startGame = function () {
     if (!interval) {
@@ -45,11 +81,28 @@ $(document).ready(function(){
   
   var questionGenerator = function () {
     var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator(10);
+    var num1 = randomNumberGenerator(numberLimit);
+    var num2 = randomNumberGenerator(numberLimit);
+    operator = getOperator();
+
+    if (operator === '/' && num2 === 0) num2 = 1;
     
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
+    if (operator === '+') {
+      question.answer = num1 + num2;
+      question.equation = String(num1) +  operator + String(num2);
+    } else if (operator === '-') {
+      if (num1 < num2) [num1, num2] = [num2, num1];
+        question.answer = num1 - num2;
+        question.equation = String(num1) + operator + String(num2);
+    } else if (operator === '*') {
+      question.answer = num1 * num2;
+      question.equation = String(num1) + operator + String(num2);
+    } else if (operator === '/') {
+      question.answer = num1;
+      question.equation = String(num1 * num2) + operator + String(num2);
+    }
+
+
     
     return question;
   };
